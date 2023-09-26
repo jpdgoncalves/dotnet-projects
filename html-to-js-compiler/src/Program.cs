5d289353ea9706ca58398bd2e3ca437ea7fa02a0
José Gonçalves <jpsextafeira@gmail.com>
2023-09-26 12:08:57 +0100

    Defining Token Types

--- html-to-js-compiler/src/Program.cs
+++ html-to-js-compiler/src/Program.cs
@@ -1,6 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-class TestOutside {
-    
+namespace HTMLToJS {
+
+    public class Program {
+        public static void Main(string[] args) {
+            string filename = "example.html";
+            string filecontent;
+
+            using (var sr = new StreamReader(filename)) {
+                filecontent = sr.ReadToEnd();
+            }
+
+
+        }
+    }
+
+    public enum TokenType {
+
+        // Used for tags
+        LEFT_ARROW,
+        LEFT_SLASH_ARROW,
+        RIGHT_ARROW,
+
+        // Control
+        SPACE,
+        DOUBLE_COLLOM,
+        EQUAL_SIGN,
+        EOF,
+
+        // Strings and text
+        ASCII_ALPHA_STRING,
+        ASCII_ALPHANUMERIC_STRING,
+        TEXT,
+    }
+
+    public class Token {
+        public TokenType Type {get;}
+        public string Value {get;}
+
+        public Token(TokenType type, string value) {
+            Type = type;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"Token {{Type={Type}, Value={Value}}}";
+        }
+    }
+
+    public class Tokenizer {
+        public Tokenizer() {
+            
+        }
+    }
 }
