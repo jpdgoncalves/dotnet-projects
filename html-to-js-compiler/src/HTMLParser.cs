30a8050b05455c521716df933e136c130ee33c5b
José Gonçalves <jpsextafeira@gmail.com>
2023-10-12 10:20:55 +0100

    Return the first root element found

--- html-to-js-compiler/src/HTMLParser.cs
+++ html-to-js-compiler/src/HTMLParser.cs
@@ -88,6 +88,11 @@ namespace HtmlToJs
         {
             walker.Root = HtmlNode.MakeTag("root");
             parser(source, 0);
+            
+            foreach (var child in walker.Root.Children) {
+                if (child.Type == HtmlNode.HTMLNodeType.TAG) return child;
+            }
+
             return walker.Root;
         }
     }
