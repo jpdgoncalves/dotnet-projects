325c10f822911d8b7d4b0944490b55488bd6522c
José Gonçalves <jpsextafeira@gmail.com>
2023-10-18 14:19:44 +0100

    More documentation

--- html-to-js-compiler/src/Scanners/Scanners.Callbacks.cs
+++ html-to-js-compiler/src/Scanners/Scanners.Callbacks.cs
@@ -6,6 +6,10 @@ namespace HtmlToJs.Scanners
     {
         public delegate void ScanFunctionCallback(string source, int start, int offset);
 
+        /// <summary>
+        /// Creates a ScanFunction which when scanner is successful, invokes the callback
+        /// with the result.
+        /// </summary>
         public static ScanFunction WithSuccess(this ScanFunction scanner, ScanFunctionCallback success)
         {
             return (string source, int start) =>
@@ -16,6 +20,13 @@ namespace HtmlToJs.Scanners
             };
         }
 
+        /// <summary>
+        /// Creates a ScanFunction which when scanner fails, invokes the callback
+        /// with the failure result.
+        /// </summary>
+        /// <param name="scanner"></param>
+        /// <param name="failure"></param>
+        /// <returns></returns>
         public static ScanFunction WithFailure(this ScanFunction scanner, ScanFunctionCallback failure)
         {
             return (string source, int start) =>
@@ -26,6 +37,10 @@ namespace HtmlToJs.Scanners
             };
         }
 
+        /// <summary>
+        /// Creates a ScanFunction which when the scanner succeeds it invokes the
+        /// success callback, and when it fails it invokes the failure callback.
+        /// </summary>
         public static ScanFunction With(this ScanFunction scanner, ScanFunctionCallback success, ScanFunctionCallback failure)
         {
             return (string source, int start) =>
