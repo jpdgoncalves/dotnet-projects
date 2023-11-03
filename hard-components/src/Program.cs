ff4bdd6f6284fd2d5894afc9b1d039c98110af9f
José Gonçalves <jpsextafeira@gmail.com>
2023-11-03 15:43:20 +0000

    Base classes

--- hard-components/src/Program.cs
+++ hard-components/src/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+public class TemplateBuilder {
+    
+}
+
+public class DirectoryBuilder {
+
+}
+
+public class FileBuilder {
+
+}
