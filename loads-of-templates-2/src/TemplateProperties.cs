902ba3a630aa96caccdb285f431b5e62c13fe1f7
José Gonçalves <jpsextafeira@gmail.com>
2023-11-16 15:07:34 +0000

    Removing needless variable

--- loads-of-templates-2/src/TemplateProperties.cs
+++ loads-of-templates-2/src/TemplateProperties.cs
@@ -6,16 +6,24 @@ namespace LoadOfTemplates {
     public class TemplateProperties {
         public required string Name;
         public required string Description;
-        public IList<TemplateParams>? Params;
+        public List<TemplateParam> Params = new();
 
         public static TemplateProperties ReadTemplateFile(string path) {
             var properties = JsonSerializer.Deserialize<TemplateProperties>(File.ReadAllText(path));
             if (properties is null) throw new Exception($"Unable to parse '{path}'!");
+            
+            // Used to check for repeated parameters
+            var temp = new HashSet<string>();
+            foreach (var param in properties.Params) {
+                if (temp.Contains(param.Name)) throw new Exception($"Param '{param.Name}' appears twice.");
+                temp.Add(param.Name);
+            }            
+
             return properties;
         }
     }
 
-    public class TemplateParams {
+    public class TemplateParam {
         public required string Name;
         public required string Description;
         public bool Required = false;
